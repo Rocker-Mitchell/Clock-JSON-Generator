12471ea29ee4fc61a528fde3bbca2f682bcfa427
Rocker-Mitchell <rocker.mat94@gmail.com>
2017-12-17 15:06:33 -0700

    Create primary module

---
+++ clock_json_generator.py
@@ -0,0 +1,23 @@
+"""Tool to generate JSON files that define clock animation.
+
+This module contains a script that will generate JSON files. The files are
+intendend to be used as resource model data for the clock in Minecraft. The
+files will be added to an 'output' folder added in the same directory as this
+module.
+
+For modularity, this module imports from a user made module to define frames and
+times to encode.
+
+Todo:
+    * Determine user made module dependencies
+    * Implement JSON file attributes
+    * Save files to output folder
+
+"""
+
+# Imports
+import json
+
+# Functions
+
+# Script
